d5045f6fff3d0d1518f09e06f8d1fb6bd8c35df3
soulwee <291373489@qq.com>
2019-08-01 13:11:13 +0800

    test

--- gitDemo/src/com/git/test/Test.java
+++ gitDemo/src/com/git/test/Test.java
@@ -3,6 +3,6 @@ package com.git.test;
 public class Test {
 	public static void main(String[] args) {
 		int i=0;
-		System.out.println("hello world!!!!");
+		//System.out.println("hello world!!!!");
 	}
 }
